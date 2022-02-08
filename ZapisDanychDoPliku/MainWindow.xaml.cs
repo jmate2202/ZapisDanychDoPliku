9565532612d7eb095871b38e7c31b375b89d51af
Mateusz Jóźwik <jmate2202@gmailcom>
2022-02-08 21:40:37 +0100

    szablon do zadania 2 dodanie widoku prowadzenie lekcji

--- ZapisDanychDoPliku/MainWindow.xaml.cs
+++ ZapisDanychDoPliku/MainWindow.xaml.cs
@@ -1,22 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using ZapisDanychDoPliku.Data;
-using ZapisDanychDoPliku.Models;
 using ZapisDanychDoPliku.Services;
 using ZapisDanychDoPliku.View;
 
@@ -46,6 +29,12 @@ namespace ZapisDanychDoPliku
         private void BT_ZmienWidokUczniowie(object sender, RoutedEventArgs e)
         {
             var nowaStrona = new UczenPage(new UczenService(daneContext));
+            Okno.Content = nowaStrona.Content;
+        }
+
+        private void BT_ZmienWidokLekcja(object sender, RoutedEventArgs e)
+        {
+            var nowaStrona = new LekcjaPage();
             Okno.Content = nowaStrona.Content;
         }
     }
